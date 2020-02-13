a46e5c03fea05f4e9321dcaf8c762f20b2e7a776
maggieumber <maggieumber@gmail.com>
2020-02-13 12:05:52 -0600

    changed sidebar to hidden unless opened

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -47,3 +47,14 @@ for (var i = 0; i < btns.length; i++) {
 		this.className += " active";
 	});
 }
+
+// Nav Overlay
+/* Set the width of the side navigation to 250px */
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
